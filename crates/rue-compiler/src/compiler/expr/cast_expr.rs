12b0e7711e95a6016e3273ca88850848d9f25fe9
Rigidity <me@rigidnetwork.com>
2024-06-24 09:05:22 -0400

    Fix cast expr

--- crates/rue-compiler/src/compiler/expr/cast_expr.rs
+++ crates/rue-compiler/src/compiler/expr/cast_expr.rs
@@ -1,22 +1,23 @@
 use rue_parser::{AstNode, CastExpr};
 
-use crate::{compiler::Compiler, ty::Value, TypeId};
+use crate::{compiler::Compiler, ty::Value};
 
 impl Compiler<'_> {
-    pub fn compile_cast_expr(&mut self, cast: &CastExpr, expected_type: Option<TypeId>) -> Value {
-        let Some(expr) = cast
-            .expr()
-            .map(|expr| self.compile_expr(&expr, expected_type))
-        else {
+    pub fn compile_cast_expr(&mut self, cast: &CastExpr) -> Value {
+        let Some(expr) = cast.expr().map(|expr| self.compile_expr(&expr, None)) else {
             return self.unknown();
         };
 
-        let ty = cast
+        let type_id = cast
             .ty()
             .map_or(self.builtins.unknown, |ty| self.compile_type(ty));
 
-        self.cast_check(expr.type_id, ty, cast.expr().unwrap().syntax().text_range());
+        self.cast_check(
+            expr.type_id,
+            type_id,
+            cast.expr().unwrap().syntax().text_range(),
+        );
 
-        Value::new(expr.hir_id, ty)
+        Value::new(expr.hir_id, type_id)
     }
 }
