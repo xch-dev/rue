50a36cf299c25a1d956b6a39c66e7b26c55be5b0
Rigidity <me@rigidnetwork.com>
2024-06-24 17:42:08 -0400

    Cleanup prefix expr

--- crates/rue-compiler/src/compiler/expr/prefix_expr.rs
+++ crates/rue-compiler/src/compiler/expr/prefix_expr.rs
@@ -8,24 +8,39 @@ use crate::{
 
 impl Compiler<'_> {
     pub fn compile_prefix_expr(&mut self, prefix_expr: &PrefixExpr) -> Value {
-        let Some(expr) = prefix_expr.expr() else {
-            return self.unknown();
+        // Determine the expected type based on the prefix operator.
+        let expected_type = match prefix_expr.op() {
+            Some(PrefixOp::Not) => Some(self.builtins.bool),
+            Some(PrefixOp::Neg) => Some(self.builtins.int),
+            None => None,
         };
 
-        let expr = self.compile_expr(&expr, None);
+        // Compile the expression.
+        let expr = prefix_expr
+            .expr()
+            .map(|expr| self.compile_expr(&expr, None))
+            .unwrap_or_else(|| self.unknown());
 
+        // If the expression has no prefix operator, return the expression as is.
+        // This is likely a parser error of some sort (in fact, it shouldn't happen).
         let Some(op) = prefix_expr.op() else {
-            return self.unknown();
+            return expr;
         };
+
+        // Check the type of the expression.
+        self.type_check(
+            expr.type_id,
+            expected_type.unwrap_or(self.builtins.unknown),
+            prefix_expr
+                .expr()
+                .map_or(prefix_expr.syntax().text_range(), |ast| {
+                    ast.syntax().text_range()
+                }),
+        );
 
         match op {
             PrefixOp::Not => {
-                self.type_check(
-                    expr.type_id,
-                    self.builtins.bool,
-                    prefix_expr.syntax().text_range(),
-                );
-
+                // Negate the expression and its type guards.
                 let mut value =
                     Value::new(self.db.alloc_hir(Hir::Not(expr.hir_id)), self.builtins.bool);
 
@@ -35,22 +50,16 @@ impl Compiler<'_> {
 
                 value
             }
-            PrefixOp::Neg => {
-                self.type_check(
-                    expr.type_id,
-                    self.builtins.int,
-                    prefix_expr.syntax().text_range(),
-                );
-
-                Value::new(
-                    self.db.alloc_hir(Hir::BinaryOp {
-                        op: BinOp::Subtract,
-                        lhs: self.builtins.nil_hir,
-                        rhs: expr.hir_id,
-                    }),
-                    self.builtins.int,
-                )
-            }
+            PrefixOp::Neg => Value::new(
+                // Subtract the expression from nil.
+                // TODO: Is there a more efficient way to do this?
+                self.db.alloc_hir(Hir::BinaryOp {
+                    op: BinOp::Subtract,
+                    lhs: self.builtins.nil_hir,
+                    rhs: expr.hir_id,
+                }),
+                self.builtins.int,
+            ),
         }
     }
 }
