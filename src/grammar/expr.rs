39be2dfec7ea84b1d39f10a45af4eb23e233191b
Rigidity <me@rigidnetwork.com>
2025-08-06 23:34:02 -0400

    Simplify expr

--- src/grammar/expr.rs
+++ src/grammar/expr.rs
@@ -1,4 +1,4 @@
-use crate::{BinaryOp, Parser, SyntaxKind, T, grammar::block::block};
+use crate::{Parser, SyntaxKind, T, grammar::block::block};
 
 pub fn expr(p: &mut Parser<'_>) {
     expr_binding_power(p, 0);
@@ -32,7 +32,7 @@ fn expr_binding_power(p: &mut Parser<'_>, minimum_binding_power: u8) {
     }
 
     loop {
-        let Some(op_kind) = p.at_any(&[
+        let Some(op) = p.at_any(&[
             T![+],
             T![-],
             T![*],
@@ -55,57 +55,29 @@ fn expr_binding_power(p: &mut Parser<'_>, minimum_binding_power: u8) {
             return;
         };
 
-        let op = match op_kind {
-            T![+] => BinaryOp::Add,
-            T![-] => BinaryOp::Subtract,
-            T![*] => BinaryOp::Multiply,
-            T![/] => BinaryOp::Divide,
-            T![%] => BinaryOp::Remainder,
-            T![<] => BinaryOp::LessThan,
-            T![>] => BinaryOp::GreaterThan,
-            T![<=] => BinaryOp::LessThanEquals,
-            T![>=] => BinaryOp::GreaterThanEquals,
-            T![==] => BinaryOp::Equals,
-            T![!=] => BinaryOp::NotEquals,
-            T![&&] => BinaryOp::And,
-            T![||] => BinaryOp::Or,
-            T![&] => BinaryOp::BitwiseAnd,
-            T![|] => BinaryOp::BitwiseOr,
-            T![^] => BinaryOp::BitwiseXor,
-            T![<<] => BinaryOp::LeftShift,
-            T![>>] => BinaryOp::RightShift,
+        let (left_binding_power, right_binding_power) = match op {
+            T![||] => (1, 2),
+            T![&&] => (3, 4),
+            T![==] | T![!=] => (5, 6),
+            T![<] | T![>] | T![<=] | T![>=] => (7, 8),
+            T![|] => (9, 10),
+            T![^] => (11, 12),
+            T![&] => (13, 14),
+            T![<<] | T![>>] => (15, 16),
+            T![+] | T![-] => (17, 18),
+            T![*] | T![/] | T![%] => (19, 20),
             _ => unreachable!(),
         };
-
-        let (left_binding_power, right_binding_power) = binding_power(op);
 
         if left_binding_power < minimum_binding_power {
             return;
         }
 
-        p.expect(op_kind);
+        p.expect(op);
 
         p.start_at(checkpoint, SyntaxKind::BinaryExpr);
         expr_binding_power(p, right_binding_power);
         p.finish();
-    }
-}
-
-fn binding_power(op: BinaryOp) -> (u8, u8) {
-    match op {
-        BinaryOp::Or => (1, 2),
-        BinaryOp::And => (3, 4),
-        BinaryOp::Equals | BinaryOp::NotEquals => (5, 6),
-        BinaryOp::LessThan
-        | BinaryOp::GreaterThan
-        | BinaryOp::LessThanEquals
-        | BinaryOp::GreaterThanEquals => (7, 8),
-        BinaryOp::BitwiseOr => (9, 10),
-        BinaryOp::BitwiseXor => (11, 12),
-        BinaryOp::BitwiseAnd => (13, 14),
-        BinaryOp::LeftShift | BinaryOp::RightShift => (15, 16),
-        BinaryOp::Add | BinaryOp::Subtract => (17, 18),
-        BinaryOp::Multiply | BinaryOp::Divide | BinaryOp::Remainder => (19, 20),
     }
 }
 
