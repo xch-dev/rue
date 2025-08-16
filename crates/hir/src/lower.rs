1265efba53b4e340fa2b8b2d67e5d44d8d6fcd28
Rigidity <me@rigidnetwork.com>
2025-08-16 00:12:25 -0400

    Implement blocks fully

--- crates/hir/src/lower.rs
+++ crates/hir/src/lower.rs
@@ -3,8 +3,8 @@ use indexmap::IndexSet;
 use rue_lir::{Lir, LirId};
 
 use crate::{
-    BinaryOp, BindingSymbol, Block, Database, DependencyGraph, Environment, FunctionSymbol, Hir,
-    HirId, Statement, Symbol, SymbolId, UnaryOp, bigint_atom,
+    BinaryOp, BindingSymbol, Database, DependencyGraph, Environment, FunctionSymbol, Hir, HirId,
+    Statement, Symbol, SymbolId, UnaryOp, bigint_atom,
 };
 
 pub fn lower_symbol(
@@ -71,7 +71,7 @@ fn lower_hir(
         Hir::Bool(value) => arena.alloc(Lir::Atom(if value { vec![1] } else { vec![] })),
         Hir::Bytes(atom) => arena.alloc(Lir::Atom(atom)),
         Hir::Reference(symbol) => arena.alloc(Lir::Path(env.path(symbol))),
-        Hir::Block(block) => lower_block(db, arena, graph, env, block),
+        Hir::Block(block) => lower_block(db, arena, graph, env, block.statements, block.body),
         Hir::Unary(op, hir) => {
             let lir = lower_hir(db, arena, graph, env, hir);
             match op {
@@ -113,53 +113,53 @@ fn lower_block(
     arena: &mut Arena<Lir>,
     graph: &DependencyGraph,
     env: &Environment,
-    mut block: Block,
+    stmts: Vec<Statement>,
+    body: Option<HirId>,
 ) -> LirId {
-    // TODO: Implement statement only blocks
+    let Some(stmt) = stmts.first() else {
+        return if let Some(body) = body {
+            lower_hir(db, arena, graph, env, body)
+        } else {
+            arena.alloc(Lir::Atom(vec![]))
+        };
+    };
 
-    let mut binding_groups = Vec::new();
-
-    while !block.statements.is_empty() {
-        let mut symbols = IndexSet::new();
-
-        while let Some(stmt) = block.statements.last()
-            && let Statement::Let(symbol) = stmt
-        {
-            symbols.insert(*symbol);
-            block.statements.pop();
-        }
-
-        binding_groups.extend(group_symbols(symbols, graph));
-
-        while let Some(stmt) = block.statements.last() {
-            match stmt {
-                Statement::Expr(_) => {
-                    block.statements.pop().unwrap();
-                }
-                Statement::Let(_) => {
-                    break;
-                }
-                Statement::If(_, _) => {
-                    todo!()
-                }
-                Statement::Return(_) => {
-                    todo!()
-                }
-                Statement::Assert(_) => {
-                    todo!()
-                }
-                Statement::Raise(_) => {
-                    todo!()
-                }
-            }
+    match stmt.clone() {
+        Statement::Let(_) => lower_let_stmts(db, arena, graph, env, stmts, body),
+        Statement::Return(hir) => lower_block(db, arena, graph, env, vec![], Some(hir)),
+        Statement::Assert(condition) => lower_assert(db, arena, graph, env, stmts, condition, body),
+        Statement::Expr(hir) => lower_block(db, arena, graph, env, stmts, body.or(Some(hir))),
+        Statement::Raise(hir) => lower_raise(db, arena, graph, env, hir),
+        Statement::If(condition, then) => {
+            lower_if(db, arena, graph, env, stmts, condition, then, body)
         }
     }
+}
+
+fn lower_let_stmts(
+    db: &Database,
+    arena: &mut Arena<Lir>,
+    graph: &DependencyGraph,
+    env: &Environment,
+    mut stmts: Vec<Statement>,
+    body: Option<HirId>,
+) -> LirId {
+    let mut symbols = IndexSet::new();
+
+    while let Some(stmt) = stmts.first()
+        && let Statement::Let(symbol) = stmt
+    {
+        symbols.insert(*symbol);
+        stmts.remove(0);
+    }
+
+    let binding_groups = group_symbols(symbols, graph);
 
     let all_bindings: Vec<SymbolId> = binding_groups.iter().rev().flatten().copied().collect();
 
     let body_env = env.with_bindings(&all_bindings);
 
-    let mut expr = lower_hir(db, arena, graph, &body_env, block.body.unwrap());
+    let mut expr = lower_block(db, arena, graph, &body_env, stmts, body);
 
     for (i, group) in binding_groups.iter().enumerate().rev() {
         expr = arena.alloc(Lir::Quote(expr));
@@ -180,6 +180,49 @@ fn lower_block(
     }
 
     expr
+}
+
+fn lower_assert(
+    db: &Database,
+    arena: &mut Arena<Lir>,
+    graph: &DependencyGraph,
+    env: &Environment,
+    stmts: Vec<Statement>,
+    condition: HirId,
+    body: Option<HirId>,
+) -> LirId {
+    let condition = lower_hir(db, arena, graph, env, condition);
+    let then_branch = lower_block(db, arena, graph, env, stmts, body);
+    let else_branch = arena.alloc(Lir::Raise(vec![]));
+    arena.alloc(Lir::If(condition, then_branch, else_branch))
+}
+
+fn lower_raise(
+    db: &Database,
+    arena: &mut Arena<Lir>,
+    graph: &DependencyGraph,
+    env: &Environment,
+    hir: HirId,
+) -> LirId {
+    let lir = lower_hir(db, arena, graph, env, hir);
+    arena.alloc(Lir::Raise(vec![lir]))
+}
+
+#[allow(clippy::too_many_arguments)]
+fn lower_if(
+    db: &Database,
+    arena: &mut Arena<Lir>,
+    graph: &DependencyGraph,
+    env: &Environment,
+    stmts: Vec<Statement>,
+    condition: HirId,
+    then: HirId,
+    body: Option<HirId>,
+) -> LirId {
+    let condition = lower_hir(db, arena, graph, env, condition);
+    let then_branch = lower_hir(db, arena, graph, env, then);
+    let else_branch = lower_block(db, arena, graph, env, stmts, body);
+    arena.alloc(Lir::If(condition, then_branch, else_branch))
 }
 
 fn group_symbols(mut symbols: IndexSet<SymbolId>, graph: &DependencyGraph) -> Vec<Vec<SymbolId>> {
