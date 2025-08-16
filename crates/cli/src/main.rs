e1e08ea8660e68fa181d3b62bee8824ffcad0d1c
Rigidity <me@rigidnetwork.com>
2025-08-16 01:20:53 -0400

    Add simple CLI

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,1 +1,43 @@
-fn main() {}
+use std::fs;
+
+use anyhow::Result;
+use clap::Parser;
+use clvm_tools_rs::classic::clvm_tools::binutils::disassemble;
+use clvmr::Allocator;
+use rue_compiler::compile_file;
+
+#[derive(Debug, Parser)]
+pub enum Command {
+    Build(BuildCommand),
+}
+
+#[derive(Debug, Parser)]
+pub struct BuildCommand {
+    file: String,
+}
+
+fn main() -> Result<()> {
+    let args = Command::parse();
+
+    match args {
+        Command::Build(cmd) => build(cmd),
+    }
+}
+
+fn build(cmd: BuildCommand) -> Result<()> {
+    let source = fs::read_to_string(cmd.file)?;
+
+    let mut allocator = Allocator::new();
+
+    let result = compile_file(&mut allocator, &source)?;
+
+    for diagnostic in result.diagnostics {
+        eprintln!("{}", diagnostic.message(&source));
+    }
+
+    if let Some(program) = result.program {
+        println!("{}", disassemble(&allocator, program, None));
+    }
+
+    Ok(())
+}
