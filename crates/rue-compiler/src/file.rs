92d6b7e3a9b034705321cd332dee163aba5a0c7d
Rigidity <me@rigidnetwork.com>
2025-08-23 22:18:01 -0400

    Basic standard library

--- crates/rue-compiler/src/file.rs
+++ crates/rue-compiler/src/file.rs
@@ -2,7 +2,7 @@ use clvmr::{Allocator, NodePtr};
 use id_arena::Arena;
 use rue_ast::{AstDocument, AstNode};
 use rue_diagnostic::{Diagnostic, DiagnosticSeverity};
-use rue_hir::{DependencyGraph, Environment, Scope, lower_symbol};
+use rue_hir::{DependencyGraph, Environment, Scope, ScopeId, lower_symbol};
 use rue_lexer::Lexer;
 use rue_lir::{Error, codegen, optimize};
 use rue_parser::Parser;
@@ -15,27 +15,22 @@ pub struct Compilation {
     pub program: Option<NodePtr>,
 }
 
+#[derive(Debug, Clone)]
+struct PartialCompilation {
+    diagnostics: Vec<Diagnostic>,
+    scope: ScopeId,
+}
+
 pub fn compile_file(allocator: &mut Allocator, file: &str) -> Result<Compilation, Error> {
-    let tokens = Lexer::new(file).collect::<Vec<_>>();
-    let parser = Parser::new(file, tokens);
-    let parse_result = parser.parse();
+    let mut ctx = Compiler::new();
+    let std = compile_file_partial(&mut ctx, include_str!("../../../std.rue"));
+    ctx.push_scope(std.scope);
+    let file = compile_file_partial(&mut ctx, file);
 
     let mut compilation = Compilation {
-        diagnostics: parse_result.diagnostics,
+        diagnostics: file.diagnostics,
         program: None,
     };
-
-    let Some(ast) = AstDocument::cast(parse_result.node) else {
-        return Ok(compilation);
-    };
-
-    let mut ctx = Compiler::new();
-
-    let scope = ctx.alloc_scope(Scope::new());
-    let declarations = declare_document(&mut ctx, scope, &ast);
-    compile_document(&mut ctx, scope, &ast, declarations);
-
-    compilation.diagnostics.extend_from_slice(ctx.diagnostics());
 
     if compilation
         .diagnostics
@@ -45,7 +40,7 @@ pub fn compile_file(allocator: &mut Allocator, file: &str) -> Result<Compilation
         return Ok(compilation);
     }
 
-    let symbol = ctx.scope(scope).symbol("main").unwrap();
+    let symbol = ctx.scope(file.scope).symbol("main").unwrap();
     let graph = DependencyGraph::build(&ctx, symbol);
 
     let mut arena = Arena::new();
@@ -63,4 +58,28 @@ pub fn compile_file(allocator: &mut Allocator, file: &str) -> Result<Compilation
     compilation.program = Some(ptr);
 
     Ok(compilation)
+}
+
+fn compile_file_partial(ctx: &mut Compiler, file: &str) -> PartialCompilation {
+    let tokens = Lexer::new(file).collect::<Vec<_>>();
+    let parser = Parser::new(file, tokens);
+    let parse_result = parser.parse();
+
+    let scope = ctx.alloc_scope(Scope::new());
+
+    let mut compilation = PartialCompilation {
+        diagnostics: parse_result.diagnostics,
+        scope,
+    };
+
+    let Some(ast) = AstDocument::cast(parse_result.node) else {
+        return compilation;
+    };
+
+    let declarations = declare_document(ctx, scope, &ast);
+    compile_document(ctx, scope, &ast, declarations);
+
+    compilation.diagnostics.extend_from_slice(ctx.diagnostics());
+
+    compilation
 }
