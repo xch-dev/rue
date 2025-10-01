40c3dc3081511974fa3e313c782ca9646b7d42d3
Rigidity <me@rigidnetwork.com>
2025-09-30 21:45:57 -0400

    rue-tests fix

--- crates/rue-tests/src/main.rs
+++ crates/rue-tests/src/main.rs
@@ -13,38 +13,39 @@ use rue_diagnostic::Source;
 use rue_diagnostic::SourceKind;
 use rue_options::CompilerOptions;
 use serde::{Deserialize, Serialize};
+use walkdir::DirEntry;
 use walkdir::WalkDir;
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Default, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(default)]
 struct TestCase {
-    #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     program: Option<String>,
 
-    #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     debug_program: Option<String>,
 
-    #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     solution: Option<String>,
 
-    #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     output: Option<String>,
 
-    #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     runtime_cost: Option<u64>,
 
-    #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     byte_cost: Option<u64>,
 
-    #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     total_cost: Option<u64>,
 
-    #[serde(default, skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     clvm_error: Option<String>,
 
-    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
     diagnostics: Vec<String>,
 
-    #[serde(default)]
     tests_passed: bool,
 }
 
@@ -68,7 +69,7 @@ fn main() -> Result<()> {
                 .join(format!("{name}.yaml"))
                 .try_exists()?
             {
-                println!("Skipping {name} because it doesn't have a test case");
+                handle_test_case(name, &entry)?;
             }
             continue;
         }
@@ -84,119 +85,133 @@ fn main() -> Result<()> {
             continue;
         }
 
-        println!("Running {name}");
-
-        let mut test_case: TestCase = serde_yml::from_str(&fs::read_to_string(entry.path())?)?;
-        let original = test_case.clone();
-
-        let source =
-            fs::read_to_string(entry.path().parent().unwrap().join(format!("{name}.rue")))?;
-
-        let source = Source::new(Arc::from(source), SourceKind::File("test".to_string()));
-
-        let mut allocator = Allocator::new();
-        let result = compile_file(&mut allocator, source.clone(), CompilerOptions::default())?;
-
-        let mut diagnostics = Vec::new();
-
-        for diagnostic in result.diagnostics {
-            diagnostics.push(diagnostic.message());
-        }
-
-        if let Some(ptr) = result.program {
-            let env = assemble(
-                &mut allocator,
-                test_case.solution.as_ref().expect("missing solution"),
-            )
-            .expect("failed to assemble env");
-
-            test_case.program = Some(disassemble(&allocator, ptr, None));
-
-            let debug_ptr = compile_file(&mut allocator, source, CompilerOptions::debug())?
-                .program
-                .unwrap();
-
-            test_case.debug_program = Some(disassemble(&allocator, debug_ptr, None));
-
-            let response = run_program(&mut allocator, &ChiaDialect::new(0), ptr, env, 100_000_000);
-            let debug_response = run_program(
-                &mut allocator,
-                &ChiaDialect::new(0),
-                debug_ptr,
-                env,
-                100_000_000,
-            );
-
-            let bytes = node_to_bytes(&allocator, ptr)?.len();
-            test_case.byte_cost = Some(bytes as u64 * 12_000);
-
-            match response {
-                Ok(output) => {
-                    let output_hash = tree_hash(&allocator, output.1);
-                    let debug_output_hash =
-                        tree_hash(&allocator, debug_response.expect("debug program failed").1);
-
-                    if output_hash != debug_output_hash {
-                        println!(
-                            "Debug output mismatch: {}",
-                            disassemble(&allocator, debug_ptr, None)
-                        );
-                    }
-
-                    test_case.output = Some(disassemble(&allocator, output.1, None));
-                    test_case.runtime_cost = Some(output.0);
-                    test_case.total_cost = Some(output.0 + bytes as u64 * 12_000);
-                    test_case.clvm_error = None;
-                }
-                Err(error) => {
-                    debug_response.expect_err("debug program succeeded unexpectedly");
-
-                    test_case.clvm_error = Some(error.to_string());
-                    test_case.runtime_cost = None;
-                    test_case.total_cost = None;
-                    test_case.output = None;
-                }
-            }
-        } else {
-            test_case.program = None;
-            test_case.debug_program = None;
-            test_case.output = None;
-            test_case.clvm_error = None;
-            test_case.runtime_cost = None;
-            test_case.byte_cost = None;
-            test_case.total_cost = None;
-        }
-
-        test_case.tests_passed = true;
-
-        for test in result.tests {
-            let response = run_program(
-                &mut allocator,
-                &ChiaDialect::new(0),
-                test,
-                NodePtr::NIL,
-                100_000_000,
-            );
-
-            if let Ok(output) = response
-                && let SExp::Atom = allocator.sexp(output.1)
-                && allocator.atom(output.1).is_empty()
-            {
-                continue;
-            }
-
-            test_case.tests_passed = false;
-            break;
-        }
-
-        test_case.diagnostics = diagnostics;
-
-        if test_case != original {
-            println!("Failed, updated test case");
-        }
-
-        fs::write(entry.path(), serde_yml::to_string(&test_case)?)?;
+        handle_test_case(name, &entry)?;
     }
+
+    Ok(())
+}
+
+fn handle_test_case(name: &str, entry: &DirEntry) -> Result<()> {
+    let parent = entry.path().parent().unwrap();
+    let yaml_file = parent.join(format!("{name}.yaml"));
+    let rue_file = parent.join(format!("{name}.rue"));
+
+    println!("Running {name}");
+
+    let mut test_case: TestCase = if yaml_file.try_exists()? {
+        serde_yml::from_str(&fs::read_to_string(&yaml_file)?)?
+    } else {
+        TestCase::default()
+    };
+
+    let original = test_case.clone();
+
+    let source = fs::read_to_string(rue_file)?;
+
+    let source = Source::new(Arc::from(source), SourceKind::File("test".to_string()));
+
+    let mut allocator = Allocator::new();
+    let result = compile_file(&mut allocator, source.clone(), CompilerOptions::default())?;
+
+    let mut diagnostics = Vec::new();
+
+    for diagnostic in result.diagnostics {
+        diagnostics.push(diagnostic.message());
+    }
+
+    if let Some(ptr) = result.program {
+        let env = assemble(
+            &mut allocator,
+            test_case.solution.as_ref().expect("missing solution"),
+        )
+        .expect("failed to assemble env");
+
+        test_case.program = Some(disassemble(&allocator, ptr, None));
+
+        let debug_ptr = compile_file(&mut allocator, source, CompilerOptions::debug())?
+            .program
+            .unwrap();
+
+        test_case.debug_program = Some(disassemble(&allocator, debug_ptr, None));
+
+        let response = run_program(&mut allocator, &ChiaDialect::new(0), ptr, env, 100_000_000);
+        let debug_response = run_program(
+            &mut allocator,
+            &ChiaDialect::new(0),
+            debug_ptr,
+            env,
+            100_000_000,
+        );
+
+        let bytes = node_to_bytes(&allocator, ptr)?.len();
+        test_case.byte_cost = Some(bytes as u64 * 12_000);
+
+        match response {
+            Ok(output) => {
+                let output_hash = tree_hash(&allocator, output.1);
+                let debug_output_hash =
+                    tree_hash(&allocator, debug_response.expect("debug program failed").1);
+
+                if output_hash != debug_output_hash {
+                    println!(
+                        "Debug output mismatch: {}",
+                        disassemble(&allocator, debug_ptr, None)
+                    );
+                }
+
+                test_case.output = Some(disassemble(&allocator, output.1, None));
+                test_case.runtime_cost = Some(output.0);
+                test_case.total_cost = Some(output.0 + bytes as u64 * 12_000);
+                test_case.clvm_error = None;
+            }
+            Err(error) => {
+                debug_response.expect_err("debug program succeeded unexpectedly");
+
+                test_case.clvm_error = Some(error.to_string());
+                test_case.runtime_cost = None;
+                test_case.total_cost = None;
+                test_case.output = None;
+            }
+        }
+    } else {
+        test_case.program = None;
+        test_case.debug_program = None;
+        test_case.output = None;
+        test_case.clvm_error = None;
+        test_case.runtime_cost = None;
+        test_case.byte_cost = None;
+        test_case.total_cost = None;
+    }
+
+    test_case.tests_passed = true;
+
+    for test in result.tests {
+        let response = run_program(
+            &mut allocator,
+            &ChiaDialect::new(0),
+            test,
+            NodePtr::NIL,
+            100_000_000,
+        );
+
+        if let Ok(output) = response
+            && let SExp::Atom = allocator.sexp(output.1)
+            && allocator.atom(output.1).is_empty()
+        {
+            continue;
+        }
+
+        test_case.tests_passed = false;
+        break;
+    }
+
+    test_case.diagnostics = diagnostics;
+
+    if test_case != original {
+        println!("Failed, updated test case");
+    }
+
+    fs::write(yaml_file, serde_yml::to_string(&test_case)?)?;
 
     Ok(())
 }
