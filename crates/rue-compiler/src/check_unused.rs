fd0aeb710b4c11221f70e2947917f459c7f4e560
Rigidity <me@rigidnetwork.com>
2025-11-03 01:01:53 -0500

    new wrong test

--- crates/rue-compiler/src/check_unused.rs
+++ crates/rue-compiler/src/check_unused.rs
@@ -11,13 +11,20 @@ use rue_types::{Alias, Generic, Struct, Type};
 use crate::Compiler;
 
 pub fn check_unused(ctx: &mut Compiler, entrypoints: &HashSet<Declaration>) {
-    let mut used_symbols = IndexSet::new();
-    let mut unused_symbols = IndexSet::new();
+    let mut used_declarations = IndexSet::new();
+    let mut unused_declarations = IndexSet::new();
 
     for declaration in ctx.relevant_declarations().collect::<Vec<_>>() {
         let Declaration::Symbol(symbol) = declaration else {
             continue;
         };
+
+        if !ctx.declaration_parents(declaration).is_empty()
+            && !ctx.reference_parents(declaration).is_empty()
+        {
+            used_declarations.insert(declaration);
+            continue;
+        }
 
         let mut visited = HashSet::new();
         let mut stack = vec![declaration];
@@ -62,38 +69,35 @@ pub fn check_unused(ctx: &mut Compiler, entrypoints: &HashSet<Declaration>) {
                 match parent {
                     Declaration::Symbol(parent_symbol) => {
                         if entrypoints.contains(&Declaration::Symbol(parent_symbol)) {
-                            used_symbols.insert(symbol);
+                            used_declarations.insert(declaration);
                         }
                     }
                     Declaration::Type(parent_ty) => {
                         if entrypoints.contains(&Declaration::Type(parent_ty)) {
-                            used_symbols.insert(symbol);
+                            used_declarations.insert(declaration);
                         }
                     }
                 }
             }
         }
 
-        if entrypoints.contains(&Declaration::Symbol(symbol)) {
-            used_symbols.insert(symbol);
+        if entrypoints.contains(&declaration) {
+            used_declarations.insert(declaration);
             continue;
         }
 
-        if !used_symbols.contains(&symbol) {
-            unused_symbols.insert(symbol);
+        if !used_declarations.contains(&declaration) {
+            unused_declarations.insert(declaration);
         }
     }
 
-    let mut used_types = IndexSet::new();
-    let mut unused_types = IndexSet::new();
-
     for declaration in ctx.relevant_declarations() {
-        let Declaration::Type(ty) = declaration else {
+        if !matches!(declaration, Declaration::Type(_)) {
             continue;
-        };
+        }
 
-        if entrypoints.contains(&Declaration::Type(ty)) {
-            used_types.insert(ty);
+        if entrypoints.contains(&declaration) {
+            used_declarations.insert(declaration);
             continue;
         }
 
@@ -110,118 +114,117 @@ pub fn check_unused(ctx: &mut Compiler, entrypoints: &HashSet<Declaration>) {
 
                 match parent {
                     Declaration::Symbol(parent_symbol) => {
-                        if used_symbols.contains(&parent_symbol) {
-                            used_types.insert(ty);
+                        if used_declarations.contains(&Declaration::Symbol(parent_symbol)) {
+                            used_declarations.insert(declaration);
                             break;
                         }
                     }
                     Declaration::Type(parent_ty) => {
                         if entrypoints.contains(&Declaration::Type(parent_ty)) {
-                            used_types.insert(ty);
+                            used_declarations.insert(declaration);
                             break;
                         }
                     }
                 }
             }
 
-            if used_types.contains(&ty) {
+            if used_declarations.contains(&declaration) {
                 break;
             }
         }
 
-        if !used_types.contains(&ty) {
-            unused_types.insert(ty);
+        if !used_declarations.contains(&declaration) {
+            unused_declarations.insert(declaration);
         }
     }
 
-    for &symbol in &unused_symbols {
-        match ctx.symbol(symbol).clone() {
-            Symbol::Unresolved | Symbol::Module(_) | Symbol::Builtin(_) => {}
-            Symbol::Function(FunctionSymbol { name, .. }) => {
-                if let Some(name) = name {
+    for &declaration in &unused_declarations {
+        match declaration {
+            Declaration::Symbol(symbol) => match ctx.symbol(symbol).clone() {
+                Symbol::Unresolved | Symbol::Module(_) | Symbol::Builtin(_) => {}
+                Symbol::Function(FunctionSymbol { name, .. }) => {
+                    if let Some(name) = name {
+                        if name.text().starts_with('_') {
+                            continue;
+                        }
+
+                        ctx.diagnostic(
+                            &name,
+                            DiagnosticKind::UnusedFunction(name.text().to_string()),
+                        );
+                    }
+                }
+                Symbol::Binding(BindingSymbol { name, .. }) => {
+                    if let Some(name) = name {
+                        if name.text().starts_with('_') {
+                            continue;
+                        }
+
+                        ctx.diagnostic(
+                            &name,
+                            DiagnosticKind::UnusedBinding(name.text().to_string()),
+                        );
+                    }
+                }
+                Symbol::Constant(ConstantSymbol { name, .. }) => {
+                    if let Some(name) = name {
+                        if name.text().starts_with('_') {
+                            continue;
+                        }
+
+                        ctx.diagnostic(
+                            &name,
+                            DiagnosticKind::UnusedConstant(name.text().to_string()),
+                        );
+                    }
+                }
+                Symbol::Parameter(ParameterSymbol { name, .. }) => {
+                    if let Some(name) = name {
+                        if name.text().starts_with('_') {
+                            continue;
+                        }
+
+                        ctx.diagnostic(
+                            &name,
+                            DiagnosticKind::UnusedParameter(name.text().to_string()),
+                        );
+                    }
+                }
+            },
+            Declaration::Type(ty) => match ctx.ty(ty).clone() {
+                Type::Generic(Generic { name: Some(name) }) => {
                     if name.text().starts_with('_') {
                         continue;
                     }
 
                     ctx.diagnostic(
                         &name,
-                        DiagnosticKind::UnusedFunction(name.text().to_string()),
+                        DiagnosticKind::UnusedGenericType(name.text().to_string()),
                     );
                 }
-            }
-            Symbol::Binding(BindingSymbol { name, .. }) => {
-                if let Some(name) = name {
+                Type::Struct(Struct {
+                    name: Some(name), ..
+                }) => {
+                    if name.text().starts_with('_') {
+                        continue;
+                    }
+
+                    ctx.diagnostic(&name, DiagnosticKind::UnusedStruct(name.text().to_string()));
+                }
+                Type::Alias(Alias {
+                    name: Some(name), ..
+                }) => {
                     if name.text().starts_with('_') {
                         continue;
                     }
 
                     ctx.diagnostic(
                         &name,
-                        DiagnosticKind::UnusedBinding(name.text().to_string()),
+                        DiagnosticKind::UnusedTypeAlias(name.text().to_string()),
                     );
                 }
-            }
-            Symbol::Constant(ConstantSymbol { name, .. }) => {
-                if let Some(name) = name {
-                    if name.text().starts_with('_') {
-                        continue;
-                    }
-
-                    ctx.diagnostic(
-                        &name,
-                        DiagnosticKind::UnusedConstant(name.text().to_string()),
-                    );
-                }
-            }
-            Symbol::Parameter(ParameterSymbol { name, .. }) => {
-                if let Some(name) = name {
-                    if name.text().starts_with('_') {
-                        continue;
-                    }
-
-                    ctx.diagnostic(
-                        &name,
-                        DiagnosticKind::UnusedParameter(name.text().to_string()),
-                    );
-                }
-            }
-        }
-    }
-
-    for &ty in &unused_types {
-        match ctx.ty(ty).clone() {
-            Type::Generic(Generic { name: Some(name) }) => {
-                if name.text().starts_with('_') {
-                    continue;
-                }
-
-                ctx.diagnostic(
-                    &name,
-                    DiagnosticKind::UnusedGenericType(name.text().to_string()),
-                );
-            }
-            Type::Struct(Struct {
-                name: Some(name), ..
-            }) => {
-                if name.text().starts_with('_') {
-                    continue;
-                }
-
-                ctx.diagnostic(&name, DiagnosticKind::UnusedStruct(name.text().to_string()));
-            }
-            Type::Alias(Alias {
-                name: Some(name), ..
-            }) => {
-                if name.text().starts_with('_') {
-                    continue;
-                }
-
-                ctx.diagnostic(
-                    &name,
-                    DiagnosticKind::UnusedTypeAlias(name.text().to_string()),
-                );
-            }
-            _ => {}
+                _ => {}
+            },
         }
     }
 }
